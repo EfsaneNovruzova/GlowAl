6ea6053186245a60cfed10faab76186d37f4efd3
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-08-19 23:52:55 +0400

    Created Entities

---
+++ src/Core/GlowAl.Domain/Entities/ProductProblem.cs
@@ -0,0 +1,10 @@
+﻿namespace GlowAl.Domain.Entities;
+
+public class ProductProblem :BaseEntity
+{
+    public Guid ProductId { get; set; }
+    public CareProduct Product { get; set; } = null!;
+
+    public Guid ProblemId { get; set; }
+    public SkinProblem Problem { get; set; } = null!;
+}
