08e1912ff2df675707fe7c16ae4464b96cd0ea20
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-08-20 21:56:15 +0400

    created dbcontext

---
+++ src/Infrastructure/GlowAl.Persistence/Contexts/GlowAlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GlowAl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlowAl.Persistence.Contexts;
+
+public class GlowAlDbContext : DbContext
+{
+    public GlowAlDbContext( DbContextOptions<GlowAlDbContext> options):base(options)
+    {  
+    }
+    
+
+    public DbSet<Article>  articles { get; set; }
+}
