4d0cd2a1912eb2931053efc3bc95b97f32e3ce72
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-08-26 20:37:04 +0400

    created dbcontext

--- src/Presentation/GlowAl.WebApi/Program.cs
+++ src/Presentation/GlowAl.WebApi/Program.cs
@@ -1,8 +1,16 @@
+using System.Threading.RateLimiting;
+using GlowAl.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<GlowAlDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));   
+});
 
 var app = builder.Build();
 
