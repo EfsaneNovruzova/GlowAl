e5fc0d08b537920c5c5ba342e87b8531ee4e45ad
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-09-01 22:06:28 +0400

    create configuration

---
+++ src/Infrastructure/GlowAl.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using GlowAl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GlowAl.Persistence.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name)
+               .IsRequired()
+               .HasMaxLength(150);
+
+        builder.Property(c => c.Description)
+               .HasMaxLength(500);
+        builder.HasOne(c => c.Parent)
+               .WithMany(c => c.SubCategories)
+               .HasForeignKey(c => c.ParentId)
+               .OnDelete(DeleteBehavior.Restrict);
+        builder.HasMany(c => c.Products)
+               .WithOne(p => p.Category)
+               .HasForeignKey(p => p.CategoryId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
+
+
+
---
+++ src/Infrastructure/GlowAl.Persistence/Configurations/ProductProblemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GlowAl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GlowAl.Persistence.Configurations;
+
+public class ProductProblemConfiguration : IEntityTypeConfiguration<ProductProblem>
+{
+    public void Configure(EntityTypeBuilder<ProductProblem> builder)
+    {
+        builder.HasKey(pp => pp.Id);
+
+        builder.HasOne(pp => pp.Product)
+               .WithMany(p => p.ProductProblems)
+               .HasForeignKey(pp => pp.ProductId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(pp => pp.Problem)
+               .WithMany(p => p.ProductProblems)
+               .HasForeignKey(pp => pp.ProblemId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
