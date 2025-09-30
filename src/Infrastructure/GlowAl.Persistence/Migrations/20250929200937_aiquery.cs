e23eb5cd50720dace9135fcc5e2b5710ad505117
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-09-30 08:52:32 +0400

    Create fileApload

---
+++ src/Infrastructure/GlowAl.Persistence/Migrations/20250929200937_aiquery.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlowAl.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class aiquery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "AIQueryHistories",
+                newName: "Prompt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Prompt",
+                table: "AIQueryHistories",
+                newName: "ProductName");
+        }
+    }
+}
---
+++ src/Presentation/GlowAl.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,30 @@
+﻿using GlowAl.Application.Abstracts.Services;
+using GlowAl.Application.DTOs.FildeDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FilesController : ControllerBase
+{
+    private readonly IFileService _fileService;
+
+    public FilesController(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload([FromForm] FileUploadRequestDto dto)
+    {
+        var url = await _fileService.UploadFileAsync(dto);
+        return Ok(new { Url = url });
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> Delete([FromQuery] string publicId)
+    {
+        var result = await _fileService.DeleteFileAsync(publicId);
+        return Ok(new { Deleted = result });
+    }
+}
