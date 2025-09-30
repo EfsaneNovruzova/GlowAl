e6a185a9c5e6fccb7ef6c1b8d993ede4cf79a402
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-09-30 21:57:40 +0400

    Created

---
+++ src/Core/GlowAl.Application/Abstracts/Repositories/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GlowAl.Application.Abstracts.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using global::GlowAl.Persistence.Repositories;
+using GlowAl.Domain.Entities;
+
+public interface IArticleRepository : IRepository<Article>
+{
+   
+}
---
+++ src/Presentation/GlowAl.WebApi/Controllers/ArticlesController.cs
@@ -0,0 +1,57 @@
+﻿using GlowAl.Application.Abstracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArticlesController : ControllerBase
+{
+    private readonly IArticleService _service;
+
+    public ArticlesController(IArticleService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] ArticleCreateDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] ArticleUpdateDto dto)
+    {
+        var result = await _service.UpdateAsync(id, dto);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _service.DeleteAsync(id);
+        return NoContent();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _service.GetByIdAsync(id);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _service.GetAllAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("filter")]
+    public async Task<IActionResult> GetByFilter([FromQuery] Guid? skinProblemId, [FromQuery] Guid? skinTypeId, [FromQuery] Guid? productId)
+    {
+        var result = await _service.GetByFilterAsync(skinProblemId, skinTypeId, productId);
+        return Ok(result);
+    }
+}
+
